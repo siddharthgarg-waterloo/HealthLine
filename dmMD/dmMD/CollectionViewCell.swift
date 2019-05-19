690f5293c4fd9abe6b9db4a3fa9386b502002d5d
coder004 <30897961+coder004@users.noreply.github.com>
2019-05-19 00:44:33 -0400

    doing things

---
+++ dmMD/dmMD/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  dmMD
+//
+//  Created by Siddharth on 18/05/19.
+//  Copyright © 2019 Siddharth. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var symptomTitle: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
